211d4ab8e4554a352c65f8dae026432c707dc20b
SC136 <swarchuri06@gmail.com>
2021-08-09 17:07:27 +0530

    Scord • A discord bot

--- Commands/Info/Help.js
+++ Commands/Info/Help.js
@@ -17,11 +17,11 @@ module.exports = {
 			.setDescription('```m\nHello there!\nhere  is some help to get u started using me.\nWe will have some fun time!```')
 			.addField('See all my categories ▸', `\`\`\`fix\n${prefix}categories\`\`\``)
 			.addField('See all my commands ▸', `\`\`\`fix\n${prefix}commands\`\`\``)
-			.addField('My links ▸', '**[Invite link](https://dsc.gg/scord "Invite me to your server!")  •  [Development Server](https://discord.gg/UVWjuAh "Join my support server")  •  [Votting link](https://vote.com "Vote for me!")**')
+			.addField('My links ▸', '**[Invite link](https://dsc.gg/scord "Invite me to your server!")  •  [Support server](https://discord.gg/UVWjuAh "Join my support server")  •  [Votting link](https://top.gg/bot/830000867205054484/vote "Vote for me")**')
 			.setImage('https://i.imgur.com/mTeojYt.png')
 			.setFooter(`Requested by ${message.author.username}  •  ©️ Scord`, message.author.avatarURL({ format: 'png', size: 1024, dynamic: true }))
 			.setColor('#2f3136')
-			.setTimestamp()
+			.setTimestamp();
 
 		message.channel.send(helpembed);
 
