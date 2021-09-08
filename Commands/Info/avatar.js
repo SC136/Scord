1d59edd2c4ac6591df1de4a24df6bcec07996eb4
SC136 <swarchuri06@gmail.com>
2021-09-08 21:36:20 +0530

    Scord • A Discord Bot

--- Commands/Info/avatar.js
+++ Commands/Info/avatar.js
@@ -13,9 +13,9 @@ module.exports = {
         const avatarEmbed = new client.discord.MessageEmbed()
             .setAuthor(client.name, client.avatar, client.url)
             .setTitle('`Avatar command`')
-            .setDescription(`\`\`\`Here is the avatar of ${user.tag} ▸`)
-            .addField(`Avatar links ▸`, `[webp](${user.avatarURL()}) • [png](${user.avatarURL({ format: `png` })}) • [jpg](${user.avatarURL({ format: `jpg` })}) • [jpeg](${user.avatarURL({ format: `jpeg` })})`)
-            .setImage(user.avatarURL({ format: `png`, size: 4096 }))
+            .setDescription(`\`\`\`Here is the avatar of ${user.tag} ▸\`\`\``)
+            .addField(`Avatar links ▸`, `[webp](${user.avatarURL()})  •  [png](${user.avatarURL({ format: `png` })})  •  [jpg](${user.avatarURL({ format: `jpg` })})  •  [jpeg](${user.avatarURL({ format: `jpeg` })})  •  [gif](https://cdn.discordapp.com/avatars/${member.id}/${member.user.avatar}.gif)`)
+            .setImage(user.avatarURL({ format: `png`, size: 4096, dynamic: true }))
             .setFooter(`Requested By ${message.author.username}  •  ©️ Scord`, message.author.avatarURL())
             .setColor(client.color)
             .setTimestamp();
